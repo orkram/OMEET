99b24f34d266b29d5de4382fff16491897cbc436
Arkadzi Zaleuski <arkansweet@gmail.com>
2021-05-21 08:49:49 +0200

    OMEET-47 | Jitsi component

---
+++ ui/orange-meet/src/app/components/jitsi/jitsi.component.spec.ts
@@ -0,0 +1,25 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {JitsiComponent} from './jitsi.component';
+
+describe('JitsiComponent', () => {
+  let component: JitsiComponent;
+  let fixture: ComponentFixture<JitsiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JitsiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JitsiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ui/orange-meet/src/app/components/jitsi/jitsi.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-jitsi',
+  templateUrl: './jitsi.component.html',
+  styleUrls: ['./jitsi.component.scss']
+})
+export class JitsiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
