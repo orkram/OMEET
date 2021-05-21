2236573710afb806996e3b5ee7003190ca434647
Arkadzi Zaleuski <arkansweet@gmail.com>
2021-05-21 08:11:15 +0200

    OMEET | Webapp deployment conf

--- ui/orange-meet/src/app/components/meetings/meetings-list/meetings-list.component.ts
+++ ui/orange-meet/src/app/components/meetings/meetings-list/meetings-list.component.ts
@@ -108,7 +108,7 @@ export class MeetingsListComponent implements OnInit, AfterViewInit{
   isExpansionDetailRow = (i: number, row: object) => row.hasOwnProperty('detailRow');
 
   joinMeeting(meeting: any): void{
-    window.open(`https://130.61.186.61/${meeting.idMeeting}#userInfo.displayName=%22${this.tokenService.getUsername()}%22`);
+    window.open(`https://130.61.186.61/${meeting.idMeeting}#userInfo.displayName=%22${this.tokenService.getEmail()}%22&config.subject="Test meeting"`);
   }
 
 }
