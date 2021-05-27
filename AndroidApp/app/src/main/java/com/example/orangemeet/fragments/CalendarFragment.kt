e85fd10498a45c2d120fc9c9d66eece7526ee01d
konrad.strek <konrad.strek@gmail.com>
2021-05-27 16:41:50 +0200

    [OMEET-138] android - add meetings to calendar; add calendar to menu; create deafult scrollview style

--- AndroidApp/app/src/main/java/com/example/orangemeet/fragments/CalendarFragment.kt
+++ AndroidApp/app/src/main/java/com/example/orangemeet/fragments/CalendarFragment.kt
@@ -1,16 +1,23 @@
 package com.example.orangemeet.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.android.volley.Response
+import com.applandeo.materialcalendarview.CalendarView
+import com.applandeo.materialcalendarview.EventDay
+import com.example.orangemeet.BackendCommunication
+import com.example.orangemeet.Meeting
 import com.example.orangemeet.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +25,75 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CalendarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var calendarView : CalendarView
+    lateinit var meetingsView : ViewGroup
+    lateinit var meetingDateView : TextView
+
+    var meetings : List<Meeting>? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        calendarView = view.findViewById(R.id.calendarView)
+        meetingsView = view.findViewById(R.id.meetingsView)
+        meetingDateView = view.findViewById(R.id.meetingDate)
+
+        meetingDateView.setText(SimpleDateFormat("dd-MM-yy").format(Calendar.getInstance().time))
+        CreateMeetingViews(Calendar.getInstance(), inflater)
+
+        val events: MutableList<EventDay> = ArrayList()
+
+        BackendCommunication.getMeetings(requireContext(),
+            Response.Listener { meetings ->
+                this.meetings = meetings
+                this.meetings!!.forEach { meeting ->
+                    val calendar = Calendar.getInstance()
+                    calendar.time = meeting.date
+                    events.add(EventDay(calendar, R.drawable.baseline_account_circle_24))
+                }
+                calendarView.setEvents(events)
+            },
+            Response.ErrorListener {
+                Toast.makeText(requireContext(), "Nie udało się załadować spotkań", Toast.LENGTH_LONG).show()
+            })
+
+        calendarView.setOnDayClickListener {eventDay ->
+            val clickedDayCalendar = eventDay.calendar
+
+            meetingDateView.setText(SimpleDateFormat("dd-MM-yy").format(clickedDayCalendar.time))
+
+            meetingsView.removeAllViews()
+
+            CreateMeetingViews(clickedDayCalendar, inflater)
+        }
+
+        return view
+    }
+
+    private fun CreateMeetingViews(clickedDayCalendar : Calendar, inflater: LayoutInflater){
+        meetings?.forEach {meeting ->
+            val meetingCalendar = Calendar.getInstance().apply { time = meeting.date }
+            if(meetingCalendar.get(Calendar.DAY_OF_YEAR) == clickedDayCalendar.get(Calendar.DAY_OF_YEAR) &&
+                meetingCalendar.get(Calendar.YEAR) == clickedDayCalendar.get(Calendar.YEAR)){
+
+                meetingsView.addView(Meeting.createView(inflater, meetingsView, meeting, null))
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
-            CalendarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            CalendarFragment()
     }
 }
